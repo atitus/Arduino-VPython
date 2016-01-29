2bfb8070fa8443d7433ad67ed865afdda8de305b
Aaron Titus <atitus@highpoint.edu>
2016-01-29 17:34:18 -0500

    updated lunar lander

--- lunarLander/lunarLander.py
+++ lunarLander/lunarLander.py
@@ -31,8 +31,8 @@ ser = serial.Serial(port, 9600, timeout=2)
 
 time.sleep(2)
 
-scene.width=700
-scene.height=700
+scene.width=1300
+scene.height=1000
 scene.range=20
 
 ground = box(pos=vector(0,-10.05,0), size=(40.0,1,1), color=color.white)
@@ -52,7 +52,11 @@ scale=5.0
 FgravArrow = arrow(pos=spaceship.pos, axis=scale*spaceship.m*g, color=color.red)
 FthrustArrow = arrow(pos=spaceship.pos, axis=Fthrust, color=color.cyan)
 
+safespeed=2
+
 voltage=[]
+scene.waitfor('click')
+
 
 while 1:
         rate(100)
@@ -65,8 +69,22 @@ while 1:
         spaceship.v = spaceship.v + (Fnet/spaceship.m)*dt
         spaceship.pos = spaceship.pos + spaceship.v*dt
         if(spaceship.pos.y-spaceship.height/2<ground.pos.y+ground.height/2):
-                print("spaceship has landed, v= ", mag(spaceship.v))
-                break
+                print("spaceship has landed, v= ",spaceship.v, "speed= ", mag(spaceship.v))
+                if(spaceship.pos.x>target.pos.x-target.size.x/2 and spaceship.pos.x<target.pos.x+target.size.x/2 and mag(spaceship.v)<safespeed and abs(spaceship.v.x)<0.5):
+                	message=label(pos=vector(0,0,0), text="You Win! The spaceship landed safely!")
+                else:
+                	message=label(pos=vector(0,0,0), text="The spaceship crashed! Bummer!")
+                	if(abs(spaceship.v.x)>1):
+                		message.text=message.text+"\nYour lateral landing speed was too large."
+                	elif(mag(spaceship.v)>safespeed):
+                		message.text=message.text+"\nYour landing speed was too large."
+                scene.waitfor('click')
+                message.visible=0
+                spaceship.v = vector(0,0,0)
+                spaceship.pos=vector(-10,8,0)
+                Fthrust=vector(0,0,0)
+                F=vector(0,0,0)
+                t=0
         t = t+dt
         FgravArrow.pos=spaceship.pos
         FgravArrow.axis=scale*Fgrav
